2132e4c7417339e45dafd2d6236703517237f28b
Haris Khan <54500301+theHarisKhan@users.noreply.github.com>
2021-11-30 13:23:01 +0500

    Update fetchAMZ
    delete empty cells or entries

--- fetchAMZ.js
+++ fetchAMZ.js
@@ -10,7 +10,6 @@ const wb = new xl.Workbook();
 const ws = wb.addWorksheet("Data");
 const headingColumnNames = [
   "Name",
-  "Link",
   "WholeSalePrice",
   "OffPrice",
   "Rating",
@@ -32,16 +31,24 @@ async function YoManScrapIt(url) {
       $(`.sg-col-inner .s-result-item.s-asin.sg-col-0-of-12.sg-col-16-of-20.AdHolder.sg-col.s-widget-spacing-small.sg-col-12-of-16 .a-section .sg-row:nth-child(2) div:nth-child(2) > .sg-col-inner,
     .sg-col-inner .s-result-item.s-asin.sg-col-0-of-12.sg-col-16-of-20.sg-col.s-widget-spacing-small.sg-col-12-of-16 .a-section .sg-row:nth-child(2) div:nth-child(2) > .sg-col-inner`).each(
         (i, el) => {
-          AMZData.push({
-            Name: $(el).find("h2 > a > span").text(),
-            Link: $(el).find("h2 > a").attr("href"),
-            WholeSalePrice: $(el).find("span.a-price-whole").text(),
-            OffPrice: $(el).find("span.a-price.a-text-price").text(),
-            Rating: $(el).find("a > i ").text(),
-            Offer: $(el)
+          if (
+            $(el).find("h2 > a > span").text() !== "" &&
+            $(el).find("span.a-price-whole").text() !== "" &&
+            $(el).find("span.a-price.a-text-price").text() !== "" &&
+            $(el)
               .find(".a-row.s-align-children-center span:nth-child(2)")
-              .text(),
-          });
+              .text() !== ""
+          ) {
+            AMZData.push({
+              Name: $(el).find("h2 > a > span").text(),
+              WholeSalePrice: $(el).find("span.a-price-whole").text(),
+              OffPrice: $(el).find("span.a-price.a-text-price").text(),
+              Rating: $(el).find("a > i ").text(),
+              Offer: $(el)
+                .find(".a-row.s-align-children-center span:nth-child(2)")
+                .text(),
+            });
+          }
         }
       );
       //   console.log(AMZData);
